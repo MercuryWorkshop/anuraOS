502927667f808d1de80610dbefc297dc54f0681d
alice <3838shah@gmail.com>
2022-10-23 15:14:00 -0400

    adding the unselector caaused a regression, revert + python

---
+++ main.py
@@ -0,0 +1,32 @@
+# Python http.server that sets Access-Control-Allow-Origin header.
+# https://gist.github.com/razor-x/9542707
+
+import os
+import sys
+import http.server
+import socketserver
+
+PORT = 8000
+
+class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
+        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+        http.server.SimpleHTTPRequestHandler.end_headers(self)
+
+def server(port):
+    httpd = socketserver.TCPServer(('', port), HTTPRequestHandler)
+    return httpd
+
+if __name__ == "__main__":
+    port = PORT
+    httpd = server(port)
+    try:
+        # os.chdir('build')
+        print("\nserving from build/ at localhost:" + str(port))
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\n...shutting down http server")
+        httpd.shutdown()
+        sys.exit()
