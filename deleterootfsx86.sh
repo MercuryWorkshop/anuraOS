1080db24c3df4fdbb9823b75f157a77c44ca2cc6
ProgrammerIn-wonderland <3838shah@gmail.com>
2023-03-19 19:53:08 -0400

    thats a funny mistake I made!

--- deleterootfsx86.sh
+++ deleterootfsx86.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd public
-rm -rf usr bin sbin lib64 libx32 debootstrap etc lib var root home sys dev proc run tmp boot
+rm -rf usr bin sbin lib64 libx32 debootstrap etc lib var root home sys dev proc run tmp boot index.list srv
