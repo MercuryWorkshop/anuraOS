56048b0af357c89f6a43656f2719cfaa4921b7ee
CoolElectronics <58010778+CoolElectronics@users.noreply.github.com>
2023-03-21 19:40:32 -0400

    adjkasdklasd

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import createServer from '@tomphttp/bare-server-node';
+import createServer from 'bare-server-node';
 import fs from "fs/promises";
 const app = express();
 const port = 8000;
@@ -16,13 +16,36 @@ app.get("/", (req: Request, res: Response) => {
 });
 app.use(async (req: Request, res: Response, next: Function) => {
 
-  res.header("Cross-Origin-Opener-Policy", "cross-origin");
+
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Resource-Policy", "same-site");
   if (req.path.endsWith("index.list")) {
     let g = await fs.readdir(__dirname + "/rootfs" + req.path.slice(0, -"index.list".length));
     res.send(g.join("\n"));
     return;
   }
   if (bare.shouldRoute(req)) {
+    //
+    //   res.header("Cross-Origin-Embedder-Policy", "require-corp");
+    //
+    //
+    //   let old = res.write.bind(res);
+    //   let write = ((chunk) => {
+    //     console.log("lol test");
+    //     console.log(chunk.toString());
+    //
+    //   }).bind(res);
+    //
+    //
+    //   // let oldsend = res.write;
+    //   // res.write = function(b) {
+    //   // oldsetheader("Cross-Origin-Embedder-Policy", "require-corp");
+    //   // oldsetheader("Access-Control-Allow-Origin", "same-site");
+    //   // oldsetheader("Access-Control-Allow-Origin", "*");
+    //   //
+    //   //   oldsend.call(this, b);
+    //   // }
+    //   res.write = write;
     bare.routeRequest(req, res);
     return;
   }
