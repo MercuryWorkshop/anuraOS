422e378c5b68a87a9cb46653ff16f20cf8088a00
ProgrammerIn-wonderland <3838shah@gmail.com>
2023-04-19 14:39:55 +0000

    rudimentary file app

--- public/fsapp.app/launchapp.js
+++ public/fsapp.app/launchapp.js
@@ -1,11 +1,11 @@
 function loadingScript(currentpath) {
-    let glxgears = AliceWM.create("glxgears")
+    let fsapp = AliceWM.create("fsapp")
 
     let iframe = document.createElement("iframe")
     iframe.style = "top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0;"
-    iframe.setAttribute("src", currentpath+"/b.html")
+    iframe.setAttribute("src", currentpath+"/filemanager.html")
 
-    glxgears.content.appendChild(iframe)
+    fsapp.content.appendChild(iframe)
 }
 
 
