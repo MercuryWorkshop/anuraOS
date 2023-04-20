d196b748032c81d42e5f3d9ae33078dd9f49fd57
ProgrammerIn-wonderland <alice@alicesworld.tech>
2023-04-20 13:49:04 +0000

    Video support

---
+++ public/libfileview.app/fileHandler.js
@@ -0,0 +1,56 @@
+window.openFile = function (path) {
+    let ext = path.split('.').slice('-1')[0] 
+    switch (ext) {
+        case 'txt':
+            openText(path)
+            break;
+        case 'mp3':
+            openAudio(path, 'audio/mpeg')
+            break;
+        case 'flac':
+            openAudio(path, 'audio/flac')
+            break;
+        case 'wav':
+            openAudio(path, 'audio/wav')
+            break;
+        case 'ogg':
+            openAudio(path, 'audio/ogg')
+            break;
+        case 'mp4':
+            openVideo(path, 'video/mp4')
+
+    }
+}
+
+function openAudio(path, mimetype) {
+    fs.readFile(path, function(err, data) {
+        let fileView = window.parent.AliceWM.create("Audio File")
+        let bloburl = URL.createObjectURL(new Blob([data]))
+        let audio = document.createElement('audio')
+        audio.src = bloburl
+        audio.setAttribute('controls', '')
+        audio.setAttribute("type", mimetype)
+        fileView.content.appendChild(audio)
+    })
+}
+function openVideo(path, mimetype) {
+    fs.readFile(path, function(err, data) {
+        let fileView = window.parent.AliceWM.create("Video File")
+        let bloburl = URL.createObjectURL(new Blob([data]))
+        let video = document.createElement('video')
+        let source = document.createElement('source')
+        source.src = bloburl
+        video.setAttribute('controls', '')
+        source.setAttribute("type", mimetype)
+        video.style = "width: 100%; height: 100%;"
+        video.appendChild(source)
+        fileView.content.appendChild(video)
+    })
+}
+
+function openText(path) {
+    fs.readFile(path, function(err, data)  {
+        let fileView = window.parent.AliceWM.create("Text Viewer");
+        fileView.content.innerText = data;
+    })
+}
