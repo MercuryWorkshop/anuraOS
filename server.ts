e311c54d31937f72ab9bea4d868ef6f29cb23c2b
r58Playz <r58playz@gmail.com>
2023-04-26 14:44:29 -0700

    remove rootfs completely since nacl doesn't use it and make stuff actually work

--- server.ts
+++ server.ts
@@ -7,7 +7,6 @@ const port = 8000;
 const bare = createServer('/bare/');
 
 
-
 app.get("/", (req: Request, res: Response) => {
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
 
@@ -15,27 +14,35 @@ app.get("/", (req: Request, res: Response) => {
   res.header("Cross-Origin-Opener-Policy", "same-origin");
   res.sendFile(__dirname + "/public/index.html");
 });
+
 app.use(async (req: Request, res: Response, next: Function) => {
-
-
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
-  res.header("Cross-Origin-Resource-Policy", "same-site");
-  if (req.path.endsWith("index.list")) {
-    let g = await fs.readdir(__dirname + "/rootfs" + req.path.slice(0, -"index.list".length));
-    res.send(g.join("\n"));
-    return;
-  }
-  if (bare.shouldRoute(req)) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
 
-    bare.routeRequest(req, res);
+  if(req.path.startsWith(__dirname + "/public")) {
+    res.sendFile(req.path);
     return;
   }
+
   next();
-})
+});
+
+app.use(async (req: Request, res: Response, next: Function) => {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+
+  if(req.path.startsWith(__dirname + "/aboutproxy/static")) {
+    res.sendFile(req.path);
+    return;
+  }
+
+  next();
+});
 
 app.use(express.static(__dirname + "/public"));
-app.use(express.static(__dirname + "/rootfs"));
 app.use(express.static(__dirname + "/aboutproxy/static"));
 
 
-app.listen(port, () => console.log(port));
+app.listen(port, () => console.log("Listening on port: ", port));
--- unfuck_symlinks.sh
+++
@@ -1,8 +0,0 @@
-# Satan: My child will create a permanent solution to the problem
-# Jesus:
-
-if [ $EUID -ne 0 ]; then
-  echo "need root :("
-  exit
-fi
-chroot rootfs /bin/bash -c "./symlinks -cr ."
