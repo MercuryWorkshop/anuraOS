9a0d6d04d5309180d89742cad5d075ed1429a026
ProgrammerIn-wonderland <alice@alicesworld.tech>
2023-04-26 14:37:45 +0000

    icons and new manifest attribute: type

--- public/chimera.js
+++ public/chimera.js
@@ -33,6 +33,13 @@ chimera = {
             document.head.appendChild(script)
         }
 
+        // Link to Google Fonts API for some reason (make this not link to external server soon)
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200';
+        document.head.appendChild(link);
+    
+
     },
     fs: undefined,
     syncRead: {
@@ -77,6 +84,9 @@ chimera = {
         })
     }
 
+
+
+
 }
 
 chimera.init()
@@ -111,7 +121,7 @@ async function registerApp(location) {
 
 
     let resp = await fetch(`${location}/manifest.json`);
-    let manifest = JSON.parse(await resp.text());
+    let manifest = await resp.json()
 
 
     let app = {
@@ -120,25 +130,36 @@ async function registerApp(location) {
         manifest,
         windowinstance: null,
         async launch() {
+            if (manifest.type == 'manual') { // This type of application is discouraged for sure but is the most powerful
+                    fetch(`${location}/${manifest.handler}`)
+                        .then(response => response.text())
+                        .then((data) => {
+                            top.window.eval(data);
+                            top.window.eval(`loadingScript("${location}")`)
+                        })
+            } else {
+                if (this.windowinstance) return;
+                let win = AliceWM.create(this.manifest.wininfo, () => {
+                    this.windowinstance = null;
+                });
 
-            if (this.windowinstance) return;
-            let win = AliceWM.create(this.manifest.wininfo, () => {
-                this.windowinstance = null;
-            });
-            let iframe = document.createElement("iframe")
-            iframe.style = "top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0;"
-            iframe.setAttribute("src", `${location}/${manifest.index}`);
+                let iframe = document.createElement("iframe")
+                iframe.style = "top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0;"
+                iframe.setAttribute("src", `${location}/${manifest.index}`);
 
-            win.content.appendChild(iframe);
+                win.content.appendChild(iframe);
 
 
-            this.windowinstance = win;
+                this.windowinstance = win;
+            }
         },
     };
     let appsContainer = $("#appsView");
     let shortcut = $("#appTemplate").content.cloneNode(true);
     shortcut.querySelector(".app-shortcut-name").innerText = manifest.name;
-
+    if (manifest["icon"]) {
+        shortcut.querySelector(".app-shortcut-image").src = `${location}/${manifest["icon"]}`
+    }
     shortcut.querySelector(".app-shortcut-image").addEventListener("click", () => {
         app.launch();
     });
@@ -156,6 +177,7 @@ window.addEventListener("load", () => {
     registerApp("term.app");
     registerApp("glxgears.app");
     registerApp("recursion.app");
+    registerApp("eruda.app")
 });
 
 
@@ -174,34 +196,3 @@ document.addEventListener("click", (e) => {
     if (e.button != 0) return;
     document.querySelector(".custom-menu").style.setProperty("display", "none");
 });
-
-// Link to Google Fonts API  
-const link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200';
-document.head.appendChild(link);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
