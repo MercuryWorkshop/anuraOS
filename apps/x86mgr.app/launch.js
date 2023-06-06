b71273b3df3419b2a7aed3145422a34ebf664f12
Alice Addison <3838shah@gmail.com>
2023-06-06 23:04:57 +0000

    make v86 launchscreen better

--- apps/x86mgr.app/launch.js
+++ apps/x86mgr.app/launch.js
@@ -10,9 +10,10 @@ function loadingScript(currentpath, app) {
   app.windowinstance = win;
 
   let screen_container = document.createElement("div");
-  screen_container.style.position = "fixed";
-  screen_container.style.width = "900px";
-  screen_container.style.height = "900px";
+  screen_container.style.position = "relative";
+  screen_container.style.width = "100%";
+  screen_container.style.height = "100%";
+  screen_container.style.backgroundColor = "#000000";
   win.content.appendChild(screen_container);
   screen_container.appendChild(document.createElement("canvas"));
   screen_container.appendChild(document.createElement("div"));
