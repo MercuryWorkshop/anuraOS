73e9094d2a39721d9af2f37f09b6e0b2c1cfb0b6
CoolElectronics <me@coolelectronics.me>
2023-06-07 23:24:03 -0400

    actually add build-debian-bin.sh

---
+++ x86_image_wizard/debian/build-debian-bin.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -veu
+
+IMAGES="$(dirname "$0")"/../../public/images
+OUT_ROOTFS_TAR="$IMAGES"/debian-rootfs.tar
+OUT_ROOTFS_BIN="$IMAGES"/debian-rootfs.bin
+OUT_ROOTFS_MNT="$IMAGES"/debian-rootfs.mntpoint
+CONTAINER_NAME=debian-full
+IMAGE_NAME=i386/debian-full
+
+rm -rf "$IMAGES/debian-boot" || :
+cp ../anurad.c .
+
+mkdir -p "$IMAGES"
+docker build . --platform linux/386 --rm --tag "$IMAGE_NAME"
+docker rm "$CONTAINER_NAME" || true
+docker create --platform linux/386 -t -i --name "$CONTAINER_NAME" "$IMAGE_NAME" bash
+
+docker export "$CONTAINER_NAME" > "$OUT_ROOTFS_TAR"
+dd if=/dev/zero "of=$OUT_ROOTFS_BIN" bs=512M count=3
+
+loop=$(sudo losetup -f)
+sudo losetup -P "$loop" "$OUT_ROOTFS_BIN"
+sudo mkfs.ext2 "$loop"
+mkdir -p "$OUT_ROOTFS_MNT"
+sudo mount "$loop" "$OUT_ROOTFS_MNT"
+
+sudo tar -xf "$OUT_ROOTFS_TAR" -C "$OUT_ROOTFS_MNT"
+
+cp -r "$OUT_ROOTFS_MNT/boot" "$IMAGES/debian-boot"
+
+sudo umount "$loop"
+sudo losetup -d "$loop"
+rm "$OUT_ROOTFS_TAR"
+rm anurad.c
+
+echo "done! created"
