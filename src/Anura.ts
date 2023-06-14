4cb6815b25526293b4191edf1ea4b2aed124962e
ProgrammerIn-wonderland <3838shah@gmail.com>
2023-06-13 22:01:03 -0400

    made window.parent.anura not needed for auto type apps

--- src/Anura.ts
+++ src/Anura.ts
@@ -86,11 +86,13 @@ class Anura {
                     let win = AliceWM.create(this.manifest.wininfo);
                     this.windowinstance = win;
 
-                    let iframe = document.createElement("iframe")
+                    let iframe = document.createElement("iframe");
                     iframe.setAttribute("style", "top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0;")
                     iframe.setAttribute("src", `${location}/${manifest.index}`);
-
                     win.content.appendChild(iframe);
+
+                    (<any>iframe.contentWindow).anura = anura;
+                    (<any>iframe.contentWindow).AliceWM = AliceWM;
                 }
             },
         };
