a705fcb8b7522e7ecce5ac4a34937f224191a89b
Logan <thecrazykid2121@gmail.com>
2023-06-15 13:44:37 -0400

    Basic ver of settings app
    Basic version of settings app, includes swappable tabs + row skeleton
    
    Will work on actually fleshing it out
    
    Co-Authored-By: rift <117926989+Riftriot@users.noreply.github.com>

---
+++ apps/settings.app/script.js
@@ -0,0 +1,16 @@
+function openSetting(evt, TabName) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(TabName).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
