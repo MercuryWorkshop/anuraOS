fee00767252c3db33411d0e93a5804fc888e8bca
ProgrammerIn-wonderland <3838shah@gmail.com>
2023-06-21 19:13:17 -0400

    make the settings app sorta do something

--- apps/settings.app/script.js
+++ apps/settings.app/script.js
@@ -1,4 +1,44 @@
 function openSetting(evt, TabName) {
+  let x86button = document.getElementById('x86subsystemsstate')
+
+  if (!(localStorage['disable-x86'] == 'true')) {
+    x86button.innerText = 'Use x86 subsystem (enabled)'  
+  } else {    
+    x86button.innerText = 'Use x86 subsystem (disabled)'
+  }
+
+  x86button.onclick = function () {
+    if (!(localStorage['disable-x86'] == 'true')) {
+      localStorage['disable-x86'] = true
+      x86button.innerText = 'Use x86 subsystem (disabled)'
+
+    } else {
+      localStorage['disable-x86'] = false
+      x86button.innerText = 'Use x86 subsystem (enabled)'
+    }
+  }
+
+  let aboutbrowserbutton = document.getElementById('abtbrowser')
+
+  if (!(localStorage['borderless-aboutbrowser'] == 'true')) {
+    aboutbrowserbutton.innerText = 'borderless about browser (disabled)'  
+  } else {    
+    aboutbrowserbutton.innerText = 'borderless about browser (enabled)'
+  }
+
+  aboutbrowserbutton.onclick = function () {
+    if (!(localStorage['borderless-aboutbrowser'] == 'true')) {
+      localStorage['borderless-aboutbrowser'] = true
+      aboutbrowserbutton.innerText = 'borderless about browser (enabled)'
+
+    } else {
+      localStorage['borderless-aboutbrowser'] = false
+      aboutbrowserbutton.innerText = 'borderless about browser (disabled)'
+    }
+  }
+
+  document.getElementById('default').click()
+  
   var i, tabcontent, tablinks;
 
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -13,4 +53,7 @@ function openSetting(evt, TabName) {
 
   document.getElementById(TabName).style.display = "flex";
   evt.currentTarget.className += " active";
+
+
+  
 }
