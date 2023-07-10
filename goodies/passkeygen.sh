62305c78e75f1393c0f5be017cea331200ad745e
AverageEmoGirl <134985726+AverageJuliet@users.noreply.github.com>
2023-07-10 09:19:53 -0400

    Updated passkeygen.sh key generation music
    you used the wrong and not based track

--- goodies/passkeygen.sh
+++ goodies/passkeygen.sh
@@ -5,7 +5,7 @@ if [ $# -ne 2 ]; then
   exit 1
 fi
 
-curl https://cdn.discordapp.com/attachments/701545519427616949/1119423885704708247/keygen_music.mp3 | paplay &
+curl https://cdn.discordapp.com/attachments/768548346998554644/1127952125247356938/KEYGEN.mp3 | paplay &
 
 echo "generating passkey, this may take a while"
 LC_ALL=C tr -dc A-Za-z0-9 </dev/random | dd if=/dev/stdin of=$2 bs=$1 count=1 conv=sync,noerror
