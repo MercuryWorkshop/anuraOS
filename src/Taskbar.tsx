00247066e5225c729ca9c933b1ed6b5c5193c87a
CoolElectronics <me@coolelectronics.me>
2023-07-15 18:05:16 -0400

    add taskbar pinned container layout, fix makefile

--- src/Taskbar.tsx
+++ src/Taskbar.tsx
@@ -1,5 +1,32 @@
+class Shortcut {
+    element: HTMLElement;
+
+    lightbar: HTMLElement;
+    constructor(svg: string, launch: () => void, appID: string) {
+        this.element = (
+            <li application={appID}>
+                <input
+                    type="image"
+                    src={svg}
+                    class="showDialog"
+                    on:click={launch}
+                />
+                <div
+                    class="lightbar"
+                    bind:lightbar={this}
+                    style="position: relative; bottom: 1px; background-color:#FFF; display:none"
+                ></div>
+                <div class="hoverMenu" style="display: none;">
+                    <ul class="openWindows"></ul>
+                </div>
+            </li>
+        );
+    }
+}
+
 class Taskbar {
-    shortcutsTray: HTMLElement;
+    activeTray: HTMLElement;
+    pinnedTray: HTMLElement;
 
     element = (
         <footer>
@@ -16,81 +43,65 @@ class Taskbar {
                     ></img>
                 </div>
             </div>
-            <nav>
-                <ul bind:shortcutsTray={this}>
+            <nav id="taskbar-bar">
+                <ul bind:activeTray={this}>
+                    <li style="height: 40px; width=40px"></li>
+                </ul>
+                <div id="taskbar-separator"></div>
+                <ul bind:pinnedTray={this}>
                     <li style="height: 40px; width=40px"></li>
                 </ul>
             </nav>
         </footer>
     );
-    constructor() {}
+
+    shortcuts: { [key: string]: Shortcut } = {};
+    constructor() { }
     addShortcut(svg: string, launch: () => void, appID: string) {
-        const elm = (
-            <li application={appID}>
-                <input
-                    type="image"
-                    src={svg}
-                    class="showDialog"
-                    on:click={launch}
-                />
-                <div
-                    class="lightbar"
-                    style="position: relative; bottom: 1px;"
-                ></div>
-                <div class="hoverMenu" style="display: none;">
-                    <ul class="openWindows"></ul>
-                </div>
-            </li>
-        );
-        this.shortcutsTray.appendChild(elm);
-        return elm;
+        const shortcut = new Shortcut(svg, launch, appID);
+        this.shortcuts[appID] = shortcut;
+        return shortcut;
     }
     killself() {
         this.element.remove();
     }
     removeShortcuts() {
-        this.element.querySelectorAll("li").forEach((element: HTMLElement) => {
-            if (element.hasAttribute("application")) element.remove();
-        });
+        for (const name in this.shortcuts) {
+            this.shortcuts[name]!.element.remove();
+            delete this.shortcuts[name];
+        }
     }
     updateTaskbar() {
         taskbar.removeShortcuts();
 
-        const orderedApps = anura.settings.get("applist");
-        const takenCareOf: any = [];
-        for (const appID in orderedApps) {
-            const appName = orderedApps[appID];
-            if (appName in anura.apps) {
-                const app = anura.apps[appName];
+        const rendered = [];
 
-                const item = taskbar.addShortcut(
-                    app.icon,
-                    app.launch.bind(app),
-                    appName,
-                );
-                if (app.windowinstance.length !== 0) {
-                    item.getElementsByClassName(
-                        "lightbar",
-                    )[0].style.backgroundColor = "#FFF";
-                }
-                takenCareOf.push(appName);
-            }
-        }
         for (const appName in anura.apps) {
             const app = anura.apps[appName];
-            console.log(appName);
-            if (
-                app.windowinstance.length !== 0 &&
-                !takenCareOf.includes(appName)
-            ) {
-                const item = taskbar.addShortcut(
+            if (app.windowinstance.length !== 0) {
+                rendered.push(appName);
+                const shortcut = taskbar.addShortcut(
                     app.icon,
                     app.launch.bind(app),
-                    appName,
+                    appName
                 );
-                item.getElementsByClassName(
-                    "lightbar",
-                )[0].style.backgroundColor = "#FFF";
+
+                this.activeTray.appendChild(shortcut.element);
+                shortcut.lightbar.style.display = "block";
+            }
+        }
+        const pinnedApps = anura.settings.get("applist");
+        for (const appName of pinnedApps) {
+            if (appName in anura.apps && !rendered.includes(appName)) {
+                const app = anura.apps[appName];
+
+                const shortcut = taskbar.addShortcut(
+                    app.icon,
+                    app.launch.bind(app),
+                    appName
+                );
+
+                this.pinnedTray.appendChild(shortcut.element);
             }
         }
     }
