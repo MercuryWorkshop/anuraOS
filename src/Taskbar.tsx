6fda456b6b0650fdf8b479ec8f262694f0c2f736
CoolElectronics <me@coolelectronics.me>
2023-07-24 13:36:06 -0400

    make unminimmizing focus, among other things

--- src/Taskbar.tsx
+++ src/Taskbar.tsx
@@ -39,6 +39,7 @@ class Taskbar {
                     bind:activeTray={this}
                     for={React.use(this.state.apps)}
                     do={(app: App) => {
+                        if (!app) return;
                         const t = (
                             <li class="taskbar-button" bind:tmp={this}>
                                 <input
@@ -68,91 +69,26 @@ class Taskbar {
                                         this.dragged = t;
                                     }}
                                     class="showDialog"
-                                    on:click-contextmenu={(e: MouseEvent) => {
-                                        if (app.windows.length > 0) {
-                                            const newcontextmenu =
-                                                new anura.ContextMenu();
-                                            newcontextmenu.addItem(
-                                                "New Window",
-                                                () => {
-                                                    app.open();
-                                                },
-                                            );
-
-                                            let winEnumerator = 0;
-                                            for (const win of app.windows) {
-                                                let displayTitle =
-                                                    win.wininfo.title;
-                                                if (win.wininfo.title === "")
-                                                    displayTitle =
-                                                        "Window " +
-                                                        winEnumerator;
-                                                newcontextmenu.addItem(
-                                                    displayTitle,
-                                                    () => {
-                                                        win.unminimize();
-                                                    },
-                                                );
-                                                winEnumerator++;
-                                            }
-                                            const pinned = anura.settings
-                                                .get("applist")
-                                                .includes(app.package);
-                                            newcontextmenu.addItem(
-                                                pinned ? "Unpin" : "Pin",
-                                                () => {
-                                                    if (pinned) {
-                                                        anura.settings.set(
-                                                            "applist",
-                                                            anura.settings
-                                                                .get("applist")
-                                                                .filter(
-                                                                    (
-                                                                        p: string,
-                                                                    ) =>
-                                                                        p !=
-                                                                        app.package,
-                                                                ),
-                                                        );
-                                                    } else {
-                                                        anura.settings.set(
-                                                            "applist",
-                                                            [
-                                                                ...anura.settings.get(
-                                                                    "applist",
-                                                                ),
-                                                                app.package,
-                                                            ],
-                                                        );
-                                                    }
-                                                },
-                                            );
-
-                                            newcontextmenu.addItem(
-                                                "Uninstall",
-                                                () => {
-                                                    alert("todo");
-                                                },
-                                            );
-                                            const c = newcontextmenu.show(
-                                                e.x,
-                                                0,
-                                            );
-                                            // HACK HACK DUMB HACK
-                                            c.style.top = "";
-                                            c.style.bottom = "69px";
-
-                                            console.log(c);
+                                    on:click={(e: MouseEvent) => {
+                                        console.log("???");
+                                        console.log(app.windows.length);
+                                        if (app.windows.length == 1) {
+                                            app.windows[0]!.focus();
                                         } else {
-                                            app.open();
+
+                                            this.showcontext(app, e);
                                         }
+
+                                    }}
+                                    on:contextmenu={(e: MouseEvent) => {
+                                        this.showcontext(app, e);
                                     }}
                                 />
                                 <div
                                     class="lightbar"
                                     style={
                                         "position: relative; bottom: 1px; background-color:#FFF; width:50%; left:50%; transform:translateX(-50%)" +
-                                        (app.windows.length == 0
+                                        (app.windows?.length == 0
                                             ? ";visibility:hidden"
                                             : "")
                                     }
@@ -181,8 +117,92 @@ class Taskbar {
         </footer>
     );
 
+    showcontext(app: App, e: MouseEvent) {
+        if (app.windows.length > 0) {
+            const newcontextmenu =
+                new anura.ContextMenu();
+            newcontextmenu.addItem(
+                "New Window",
+                () => {
+                    app.open();
+                },
+            );
+
+            let winEnumerator = 0;
+            for (const win of app.windows) {
+                let displayTitle =
+                    win.wininfo.title;
+                if (win.wininfo.title === "")
+                    displayTitle =
+                        "Window " +
+                        winEnumerator;
+                newcontextmenu.addItem(
+                    displayTitle,
+                    () => {
+                        win.focus();
+                        win.unminimize();
+                    },
+                );
+                winEnumerator++;
+            }
+            const pinned = anura.settings
+                .get("applist")
+                .includes(app.package);
+            newcontextmenu.addItem(
+                pinned ? "Unpin" : "Pin",
+                () => {
+                    if (pinned) {
+                        anura.settings.set(
+                            "applist",
+                            anura.settings
+                                .get("applist")
+                                .filter(
+                                    (
+                                        p: string,
+                                    ) =>
+                                        p !=
+                                        app.package,
+                                ),
+                        );
+                    } else {
+                        anura.settings.set(
+                            "applist",
+                            [
+                                ...anura.settings.get(
+                                    "applist",
+                                ),
+                                app.package,
+                            ],
+                        );
+                    }
+                },
+            );
+
+            newcontextmenu.addItem(
+                "Uninstall",
+                () => {
+                    alert("todo");
+                },
+            );
+            const c = newcontextmenu.show(
+                e.x,
+                0,
+            );
+            // HACK HACK DUMB HACK
+            c.style.top = "";
+            c.style.bottom = "69px";
+
+            console.log(c);
+        } else {
+            app.open();
+        }
+
+    }
+
+
+
     // shortcuts: { [key: string]: Shortcut } = {};
-    constructor() {}
+    constructor() { }
     addShortcut(app: App) {
         // const shortcut = new Shortcut(app);
         // this.shortcuts[app.package] = shortcut;
