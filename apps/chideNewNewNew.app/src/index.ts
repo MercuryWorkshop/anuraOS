f4961d957117ad1931354c7dc3c4f8eae1d2e5c9
ProgrammerIn-wonderland <3838shah@gmail.com>
2023-08-01 20:17:58 +0400

    introducing, ChideNewNewNew.app

---
+++ apps/chideNewNewNew.app/src/index.ts
@@ -0,0 +1,17 @@
+/// <reference path="../../../src/Anura.ts" /> 
+
+import * as monaco from "monaco-editor";
+function start(){
+    // const model = monaco.editor.createModel(value, language, path);
+    
+    let editor = monaco.editor.create(document.querySelector('#container')!, {
+        value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+        language: 'javascript'
+      });
+    //   editor.language
+
+
+    let anura:Anura = (window.top as any).anura;
+}
+
+window.onload = start;
