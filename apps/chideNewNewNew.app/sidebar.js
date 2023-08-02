8890b3bbfd26742c10372d7a96f0fb21668ca9e1
ProgrammerIn-wonderland <3838shah@gmail.com>
2023-08-03 00:04:09 +0400

    splitting parts of chide more

--- apps/chideNewNewNew.app/sidebar.js
+++ apps/chideNewNewNew.app/sidebar.js
@@ -1,6 +1,11 @@
 let anura = window.parent.anura;
-
 window.fs = anura.fs
+
+/**
+ * 
+ * @param {HTMLElement} parent 
+ * @param {string} path 
+ */
 function loadPath(parent, path) {
     console.debug("loading path: ", path);
     fs.readdir(path, (err, files) => {
@@ -11,54 +16,75 @@ function loadPath(parent, path) {
 
             fs.stat(`${path}/${file}`, function(err, stats) {
                 if (err) throw err;
-                if (stats.isDirectory()) {
-                    let name = document.createElement("span")
-                    name.innerText = `${file}/`
-
-                    name.addEventListener("click", function() {
-
-                        if (item.getAttribute("expanded") == "false"){
-                            console.debug("Expanding")
-                            let childContainer = document.createElement("ul")
-
-                            loadPath(childContainer, `/${path}/${file}/`)
-                            item.appendChild(childContainer)
-                            item.setAttribute("expanded", "true")
-                        } else {
-                            item.removeChild(item.getElementsByTagName("ul")[0])
-                            item.setAttribute("expanded", "false")
-                        }
-                    });
-                    item.appendChild(name)
-                    item.setAttribute("expanded", "false")
-                    item.setAttribute("data-type", 'dir');
-                    item.setAttribute("data-path", `${path}/${file}`);
-                } else {
-                    item.innerText = `${file}`
-                    item.addEventListener("click", function() {
-                        anura.fs.readFile(`${path}/${file}`, function (err, data) {
-                            if (err) throw err;
-                            window.currentlyOpenFile = `${path}/${file}`
-                            window.editor = monaco.editor.create(document.getElementById('monaco-container'), {
-                                value: new TextDecoder().decode(data),
-                                    theme: "vs-dark",
-                                    automaticLayout: true,
-                                    language: getFileType(file)
-                            });
-                            window.editor.getModel().onDidChangeContent((event) => {
-                                if (window.currentlyOpenFile)
-                                    anura.fs.writeFile(window.currentlyOpenFile, editor.getValue())
-                            });
-                            
-                        })
-                    })
-                    item.setAttribute("data-type", 'file');
-                    item.setAttribute("data-path", `${path}/${file}`);
-                }
+                createRow(item, stats.isDirectory(), path, file)
                 console.debug("appending");
                 parent.appendChild(item)
             });
         });
     });
 }
+
+/**
+ * 
+ * @param {string} path 
+ * @param {string} file
+ */
+function loadFile(path, file) {
+    anura.fs.readFile(`${path}/${file}`, function (err, data) {
+        if (err) throw err;
+        window.currentlyOpenFile = `${path}/${file}`
+        window.editor = monaco.editor.create(document.getElementById('monaco-container'), {
+            value: new TextDecoder().decode(data),
+                theme: "vs-dark",
+                automaticLayout: true,
+                language: getFileType(file)
+        });
+        window.editor.getModel().onDidChangeContent((event) => {
+            if (window.currentlyOpenFile)
+                anura.fs.writeFile(window.currentlyOpenFile, editor.getValue())
+        });
+        
+    })
+}
+
+/**
+ * 
+ * @param {HTMLElement} RowElement 
+ * @param {boolean} isFolder 
+ * @param {string} path 
+ * @param {string} file 
+ */
+function createRow(RowElement, isFolder, path, file) {
+    if (isFolder) {
+
+        let name = document.createElement("span")
+        name.innerText = `${file}/`
+
+        name.addEventListener("click", function() {
+            if (RowElement.getAttribute("expanded") == "false"){
+                console.debug("Expanding")
+                let childContainer = document.createElement("ul")
+
+                loadPath(childContainer, `/${path}/${file}/`)
+                RowElement.appendChild(childContainer)
+                RowElement.setAttribute("expanded", "true")
+            } else {
+                item.removeChild(item.getElementsByTagName("ul")[0])
+                item.setAttribute("expanded", "false")
+            }
+        });
+        RowElement.appendChild(name)
+        RowElement.setAttribute("expanded", "false")
+        RowElement.setAttribute("data-type", 'dir');
+        RowElement.setAttribute("data-path", `${path}/${file}`);
+    } else {
+        RowElement.innerText = `${file}`
+        RowElement.addEventListener("click", function() {
+            loadFile(path, file);
+        })
+        RowElement.setAttribute("data-type", 'file');
+        RowElement.setAttribute("data-path", `${path}/${file}`);
+    }
+}
+
 loadPath(document.getElementById("root"), "/")
