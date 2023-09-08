c4a1031026a8abec7baee0f16a56e89c458dd3c0
ProgrammerIn-wonderland <3838shah@gmail.com>
2023-09-08 18:53:23 -0400

    remove legacy unused apps

--- apps/zsp.app/launchapp.js
+++
@@ -1,9 +0,0 @@
-function loadingScript(currentpath) {
-    let zsp = AliceWM.create("ZSP interpreter")
-
-    let iframe = document.createElement("iframe")
-    iframe.style = "top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0;"
-    iframe.setAttribute("src", "zsp.app/playground.html")
-
-    zsp.content.appendChild(iframe)
-}
