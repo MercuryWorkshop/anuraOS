3a0a884b6b539a5d3f8bcc5fed5ab9c9f78c1960
Percs <83934299+Percslol@users.noreply.github.com>
2023-09-10 05:29:23 -0500

    fixed keyboard and xfrog issues

--- x86_image_wizard/arch/anura-boot.sh
+++ x86_image_wizard/arch/anura-boot.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 mount -t 9p host9p /root &
-rmmod ne2k-pci && modprobe ne2k-pci
+while ! ip link | grep -q "enp0s5"; do
+    echo "interface not found. waiting"
+    sleep 5
+done
+
 ifconfig enp0s5 192.168.1.5 netmask 255.255.255.0 up
 route add default gw 192.168.1.1
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
-
-sleep 60
-dhcpcd &
