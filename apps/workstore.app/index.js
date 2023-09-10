a7670f0d3b9bc7639dff9e72a3c51dd7554a5f31
ProgrammerIn-wonderland <3838shah@gmail.com>
2023-09-10 04:54:44 -0400

    load icons through bare async

--- apps/workstore.app/index.js
+++ apps/workstore.app/index.js
@@ -28,7 +28,11 @@ async function loadappListScreen(repo) {
         const itemText = document.createElement('span')
         
         
-        thumbnail.src = repos[repo] + repoItems['apps'][item]['icon']
+        
+        (async () => {
+            thumbnail.src = URL.createObjectURL(await (await fetch(repos[repo] + repoItems['apps'][item]['icon'])).blob())
+        })()
+
         itemText.innerText = repoItems['apps'][item]['name']
         app.title = repoItems['apps'][item]['desc'] // idk why the tooltip is called title but whatever
         app.className = 'app'
