ac57cfac99798c3b8a14fa2019350ba6eadb59e3
Madjik <madjiktv58@gmail.com>
2023-09-13 01:17:47 +0000

    Style.app

---
+++ apps/style.app/index.js
@@ -0,0 +1,7 @@
+function changeWallpaper(wallpaper) {
+    var back = "url("+wallpaper+")";
+    window.parent.document.body.style.background = back;
+    window.parent.document.body.style.backgroundSize = "cover";
+    window.parent.document.body.style.backgroundPositionY = "center";
+}
+// background: url(/assets/wallpaper/touhou.jpg) no-repeat center center
