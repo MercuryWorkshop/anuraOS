8bbf0fd83d4ec1a0f54a2f0833a8a5148160bdbc
Percs <83934299+Percslol@users.noreply.github.com>
2023-09-22 17:48:03 -0500

    system boot time optimization and mounting /root early

--- x86_image_wizard/arch/anura-boot.sh
+++ x86_image_wizard/arch/anura-boot.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-mount -t 9p host9p /root &
 while ! ip link | grep -q "enp0s5"; do
     echo "interface not found. waiting"
     sleep 5
 done
 
-ifconfig enp0s5 192.168.1.5 netmask 255.255.255.0 up
-route add default gw 192.168.1.1
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
+dhcpcd -b
