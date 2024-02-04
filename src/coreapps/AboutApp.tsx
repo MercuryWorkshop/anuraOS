2b08aec9ef8e67ca9c5952f3c3df4ed71858d09c
endercass <46408156+Endercass@users.noreply.github.com>
2024-02-03 21:45:02 -0500

    Add fullscreen easteregg to aboutapp

--- src/coreapps/AboutApp.tsx
+++ src/coreapps/AboutApp.tsx
@@ -58,14 +58,53 @@ class AboutApp extends App {
         super();
     }
 
-    async open(): Promise<WMWindow | undefined> {
+    async open(args: string[] = []): Promise<WMWindow | undefined> {
+        let fullscreenEasterEgg = false;
+
+        if (args.length > 0) {
+            if (args.includes("fullscreen-easter-egg")) {
+                fullscreenEasterEgg = true;
+            }
+            if (args.includes("fuller-screen-easter-egg")) {
+                // You asked for it
+                document.body.style.background =
+                    "url(/assets/images/lagtrain.gif) no-repeat center center fixed";
+
+                anura.wm.windows.forEach((win) => {
+                    // No animation
+                    win.deref()!.element.style.display = "none";
+                    win.deref()!.close();
+                });
+
+                taskbar.element.remove();
+
+                document.title = "Lagtrain";
+
+                const icon = document.querySelector(
+                    "link[rel~='icon']",
+                )! as HTMLLinkElement;
+
+                icon.type = "image/gif";
+                icon.href = "/assets/images/lagtrain.gif";
+
+                return;
+            }
+        }
+
         const aboutview = anura.wm.create(this, {
             title: "",
             width: "400px",
-            height: "450px",
+            height: fullscreenEasterEgg ? "400px" : "450px",
+            resizable: false,
         });
 
-        aboutview.content.appendChild(await this.page());
+        if (fullscreenEasterEgg) {
+            aboutview.content.appendChild(
+                <div style="background: url(/assets/images/lagtrain.gif); width: 100%; height: 100%; background-size: contain; background-repeat: no-repeat;"></div>,
+            );
+        } else {
+            aboutview.content.appendChild(await this.page());
+        }
 
         // make borderless
         aboutview.content.style.position = "absolute";
