bb65732b4badfd50959b10499db19657c0f78a9d
endercass <46408156+Endercass@users.noreply.github.com>
2024-02-10 22:08:52 -0500

    Add JSDoc comments to app manifest

--- src/Anura.ts
+++ src/Anura.ts
@@ -199,13 +199,48 @@ class Anura {
 }
 
 interface AppManifest {
+    /**
+     * The name of the app.
+     */
     name: string;
+    /**
+     * The type of the app. This can be "manual" or "auto". If it is "manual", the app will be handled by the
+     * handler specified in the handler field. If it is "auto", the app will be handled by the index file
+     * specified in the index field. If the type is not "manual" or "auto", it will be handled by the anura
+     * library specified in the type field.
+     */
     type: "manual" | "auto" | string;
+    /**
+     * The package name of the app. This should be unique to the app and should be in reverse domain notation.
+     * For example, if the app is called "My App" and is made by "My Company", the package name should be
+     */
     package: string;
+    /**
+     * The index file for the app. This is the file that will be loaded when the app is launched when the app
+     * is in auto mode.
+     */
     index?: string;
+    /**
+     * The icon for the app. This should be a URL to an image file.
+     */
     icon: string;
+    /**
+     * The handler for the app. This is the file that will be loaded when the app is launched when the app
+     * is in manual mode.
+     */
     handler?: string;
+    /**
+     * Whether or not the app should be hidden from the app list. This is useful for apps that are
+     * only meant to be launched by other apps.
+     */
     hidden?: boolean;
+    /**
+     * The background color of the element directly behind the app's window. This is optional and defaults
+     * to the system theme's background color.
+     */
     background?: string;
+    /**
+     * This contains the properties for the default app window.
+     */
     wininfo: string | WindowInformation;
 }
