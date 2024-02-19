7fd62f09f14a1545cd15fcacaaf330c9e7866636
endercass <46408156+Endercass@users.noreply.github.com>
2024-02-19 18:38:14 -0500

    Remove old Shell override

--- src/api/Filesystem.ts
+++ src/api/Filesystem.ts
@@ -926,13 +926,6 @@ class AnuraFilesystem implements AnuraFSOperations<any> {
     constructor(providers: AFSProvider<any>[]) {
         providers.forEach((provider) => {
             this.providers.set(provider.domain, provider);
-            if (provider.domain === "/") {
-                // The Shell is defined by the root filesystem. This is a bit of a hack
-                // and should potentially be manually reimplemented or outright removed
-                // in the next major version. It will not yet be deprecated, but it does
-                // have the potential to be removed in the future.
-                this.Shell = (provider as FilerAFSProvider).fs.Shell;
-            }
         });
     }
 
