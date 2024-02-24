04fb389d1e45094c0dd53dfbb3b70bbfd8e0fb92
Percs <83934299+Percslol@users.noreply.github.com>
2024-02-24 11:59:48 -0600

    fix issue where non stale apps would be removed

--- src/Anura.ts
+++ src/Anura.ts
@@ -150,8 +150,8 @@ class Anura {
         for (const appName in anura.apps) {
             const app = anura.apps[appName];
             app.windows.forEach((win: any) => {
-                if (!win.element.parentElement) {
-                    app.windows.splice(app.windows.indexOf(win));
+                if (win.element.parentElement == null) {
+                    app.windows.splice(app.windows.indexOf(win), 1);
                 }
             });
         }
