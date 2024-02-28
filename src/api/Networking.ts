6c393d02ed46531b4b6cf25799ba46d0d75ca619
Percs <83934299+Percslol@users.noreply.github.com>
2024-02-27 18:53:00 -0600

    slim down networking api

--- src/api/Networking.ts
+++ src/api/Networking.ts
@@ -1,6 +1,5 @@
 class Networking {
     libcurl: any;
-    WebSocket: any;
     constructor(wisp_server: string) {
         //@ts-ignore
         import("/libcurl.mjs").then((m) => {
@@ -9,49 +8,13 @@ class Networking {
         });
         document.addEventListener("libcurl_load", () => {
             this.libcurl.set_websocket(wisp_server);
-            this.WebSocket = this.libcurl.WebSocket;
+            Object.assign(this, {
+                WebSocket: this.libcurl.WebSocket,
+                fetch: this.libcurl.fetch,
+            });
             console.log("libcurl.js ready!");
         });
     }
-    Socket = class Socket {
-        constructor() {}
-        ondata(data: any) {}
-        onconnect() {}
-        on(type: string, callback: () => void) {
-            switch (type) {
-                case "data":
-                    this.ondata = callback;
-                    break;
-                case "connect":
-                    this.onconnect = callback;
-                    break;
-            }
-        }
-        write(data: any) {
-            this.websocket.send(data);
-        }
-        websocket: WebSocket;
-        readyState = false;
-        connecting = true;
-        connect(port: number, host: string) {
-            this.websocket = new WebSocket(
-                anura.wsproxyURL + `${host}:${port}`,
-                ["binary"],
-            );
-            this.websocket.onmessage = async (event) => {
-                const data = Filer.Buffer.from(await event.data);
-                this.ondata(data);
-            };
-            this.websocket.onopen = (event) => {
-                this.readyState = true;
-                this.connecting = false;
-                // webSocket.send('GET / HTTP/1.1\r\nHost: alicesworld.tech\r\n\r\n');
-            };
-        }
-    };
-    fetch = async function (resource: URL | string, options: object) {
-        return await this.libcurl.fetch(resource, options);
-    };
     setWispServer = function (wisp_server: string) {
         this.libcurl.set_websocket(wisp_server);
     };
