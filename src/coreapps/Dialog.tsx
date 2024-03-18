5aede650c2af598b22f8c936e1918bf9c0a2c759
Percs <83934299+Percslol@users.noreply.github.com>
2024-03-18 13:31:26 -0500

    extend dialog api more

--- src/coreapps/Dialog.tsx
+++ src/coreapps/Dialog.tsx
@@ -9,23 +9,56 @@ class Dialog extends App {
         super();
     }
 
-    async dialog(
-        title: string,
-        description: string,
-    ): Promise<WMWindow | undefined> {
-        const win = anura.wm.create(this, this as object);
-
-        const h2 = document.createElement("h2");
-        h2.textContent = title;
-        win.content.appendChild(h2);
-        const p = document.createElement("p");
-        p.textContent = description;
-        win.content.appendChild(p);
-
+    async alert(title: string, description: string): Promise<void> {
+        const dialog = this as object;
+        (dialog as any).title = title;
+        const win = anura.wm.create(this, dialog);
         const matter = document.createElement("link");
         matter.setAttribute("rel", "stylesheet");
         matter.setAttribute("href", "/assets/matter.css");
-
-        return win;
+        win.content.appendChild(matter);
+        const titleElement = document.createElement("h2");
+        titleElement.textContent = title;
+        win.content.appendChild(titleElement);
+        const descriptionElement = document.createElement("p");
+        descriptionElement.textContent = description;
+        win.content.appendChild(descriptionElement);
+        const closeButton = document.createElement("button");
+        closeButton.addEventListener("click", (event) => {
+            win.close();
+        });
+        closeButton.textContent = "OK";
+        win.content.appendChild(closeButton);
+    }
+    async confirm(title: string, description: string): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            const dialog = this as object;
+            (dialog as any).title = title;
+            const win = anura.wm.create(this, dialog);
+            const matter = document.createElement("link");
+            matter.setAttribute("rel", "stylesheet");
+            matter.setAttribute("href", "/assets/matter.css");
+            win.content.appendChild(matter);
+            const titleElement = document.createElement("h2");
+            titleElement.textContent = title;
+            win.content.appendChild(titleElement);
+            const descriptionElement = document.createElement("p");
+            descriptionElement.textContent = description;
+            win.content.appendChild(descriptionElement);
+            const cancelButton = document.createElement("button");
+            cancelButton.addEventListener("click", (event) => {
+                resolve(false);
+                win.close();
+            });
+            cancelButton.textContent = "Cancel";
+            win.content.appendChild(cancelButton);
+            const confirmButton = document.createElement("button");
+            confirmButton.addEventListener("click", (event) => {
+                resolve(true);
+                win.close();
+            });
+            confirmButton.textContent = "OK";
+            win.content.appendChild(confirmButton);
+        });
     }
 }
