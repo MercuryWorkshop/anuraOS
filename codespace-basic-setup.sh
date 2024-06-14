4d1804b9593fbb67199011c4617182c87634f8bb
FunnyLoaf <corgloaf@gmail.com>
2024-06-14 13:31:26 -0400

    Update codespace-basic-setup.sh

--- codespace-basic-setup.sh
+++ codespace-basic-setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+#fix the uuidgen command not found error
+sudo apt update
+sudo apt-get install uuid-runtime
 
+#install
 git submodule update --init
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env" # to import rustup in current shell
 make all
+
+#start server
+cd server
+npm start -- --auth
