2c1b71b140dbc6037e1362f2ac7843aaf82e9f3a
FunnyLoaf <corgloaf@gmail.com>
2024-06-15 10:49:46 -0400

    Update codespace-basic-setup.sh

--- codespace-basic-setup.sh
+++ codespace-basic-setup.sh
@@ -10,5 +10,6 @@ source "$HOME/.cargo/env" # to import rustup in current shell
 make all
 
 #start server
-cd server
-npm start -- --auth
+#cd server
+#npm start -- --auth
+#auth appears to be deprecated in this version, removing start section as suggested
