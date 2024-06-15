c7c67d58090264c04296c7f34b68f03ec46492ab
FunnyLoaf <corgloaf@gmail.com>
2024-06-15 10:53:44 -0400

    remove start section of codespace-basic-setup.sh

--- codespace-basic-setup.sh
+++ codespace-basic-setup.sh
@@ -8,8 +8,3 @@ git submodule update --init
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env" # to import rustup in current shell
 make all
-
-#start server
-#cd server
-#npm start -- --auth
-#auth appears to be deprecated in this version, removing start section as suggested
