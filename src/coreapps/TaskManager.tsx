2bdca3a816a561d1223861d044fb48feffa775b9
BomberFish <bomberfish@bomberfish.ca>
2024-07-29 16:39:09 -0400

    task manager as a table

--- src/coreapps/TaskManager.tsx
+++ src/coreapps/TaskManager.tsx
@@ -1,3 +1,4 @@
+// probably some of the most clusterfucked code i've written - fish
 class TaskManager extends App {
     constructor() {
         super();
@@ -6,15 +7,41 @@ class TaskManager extends App {
         this.package = "anura.taskmgr";
     }
 
+    // incomprehensible css
     css = css`
         overflow: hidden;
 
         .row {
             padding-inline: 1em;
-            // width: 100%;
             height: 1.75rem;
-            display: flex;
-            align-items: center;
+
+            &,
+            & td,
+            & th {
+                &:not(:first-of-type):not(last-of-type) {
+                    border: 2px solid var(--theme-border);
+                }
+
+                height: 1.75rem;
+                line-height: 1.75rem;
+            }
+
+            border-right: 0 !important;
+            border-left: 0 !important;
+
+            & > td {
+                padding-inline: 0.5rem;
+                padding-left: calc(0.5rem + 5px); /* account for spacer */
+
+                & > span {
+                    display: flex;
+                    align-items: center;
+
+                    &.pid {
+                        justify-content: center;
+                    }
+                }
+            }
         }
 
         .icon {
@@ -28,12 +55,36 @@ class TaskManager extends App {
             color: var(--theme-fg);
         }
 
-        .list {
-            overflow-y: scroll;
+        .listwrapper {
             height: calc(100% - 8rem);
             border: 1px var(--theme-border) solid;
             margin: 0.5rem;
+            width: calc(100% - 1rem);
+            overflow-y: scroll;
+        }
+
+        .list {
+            max-height: calc(100% - 8rem);
             margin-bottom: 0;
+            border-collapse: collapse;
+            width: 100%;
+
+            & tbody {
+                max-height: calc(100% - 1rem);
+                overflow-y: scroll;
+            }
+
+            & thead {
+                height: 1rem;
+                position: sticky;
+                top: 0;
+                background: var(--theme-bg);
+                border-bottom: 2px solid var(--theme-border);
+            }
+
+            & th {
+                position: relative;
+            }
         }
 
         .controls {
@@ -42,6 +93,37 @@ class TaskManager extends App {
             align-items: center;
             justify-content: flex-end;
             padding: 0.5rem;
+        }
+
+        .resizer {
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: 5px;
+            cursor: ew-resize;
+            user-select: none;
+            border-right: 2px solid var(--theme-border);
+        }
+        .resizer:hover {
+            border-color: color-mix(
+                in srgb,
+                var(--theme-border) 80%,
+                var(--theme-fg)
+            );
+        }
+
+        .resizing {
+            border-color: color-mix(
+                in srgb,
+                var(--theme-border) 60%,
+                transparent
+            );
+        }
+
+        .proc-title {
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
         }
     `;
 
@@ -60,45 +142,68 @@ class TaskManager extends App {
             }}
             class={`background ${this.css}`}
         >
-            <div class="list">
-                {use(anura.processes.state.procs, (procs) =>
-                    procs.map((proc) => (
-                        <div
-                            class="row"
-                            class:selected={use(
-                                this.state.selected,
-                                (num) => num == proc.deref()?.pid,
-                            )}
-                            on:click={() => {
-                                console.log("setting");
-                                if (proc.deref()) {
-                                    this.state.selected = proc.deref()!.pid;
-                                }
-                                console.log(this.state.selected);
-                            }}
-                        >
-                            <img
-                                alt="app"
-                                class="icon"
-                                src={(() => {
-                                    let icon = anura.apps["anura.generic"].icon;
-                                    const process = proc.deref();
-                                    if (
-                                        process instanceof WMWindow &&
-                                        process.app
-                                    ) {
-                                        icon = process.app.icon;
-                                    }
-                                    return icon;
-                                })()}
-                            />
+            <div class="listwrapper">
+                <table class="list">
+                    <thead>
+                        <th>Name</th>
+                        <th>PID</th>
+                    </thead>
+                    <tbody>
+                        {use(anura.processes.state.procs, (procs) =>
+                            procs.map((proc) => (
+                                <tr
+                                    class="row"
+                                    class:selected={use(
+                                        this.state.selected,
+                                        (num) => num == proc.deref()?.pid,
+                                    )}
+                                    on:click={() => {
+                                        console.log("setting");
+                                        if (proc.deref()) {
+                                            this.state.selected =
+                                                proc.deref()!.pid;
+                                        }
+                                        console.log(this.state.selected);
+                                    }}
+                                >
+                                    <td>
+                                        <span>
+                                            <img
+                                                alt="app"
+                                                class="icon"
+                                                src={(() => {
+                                                    let icon =
+                                                        anura.apps[
+                                                            "anura.generic"
+                                                        ].icon;
+                                                    const process =
+                                                        proc.deref();
+                                                    if (
+                                                        process instanceof
+                                                            WMWindow &&
+                                                        process.app
+                                                    ) {
+                                                        icon = process.app.icon;
+                                                    }
+                                                    return icon;
+                                                })()}
+                                            />
+                                            <span class="proc-title">
+                                                {proc.deref()?.title}
+                                            </span>
+                                        </span>
+                                    </td>
 
-                            <span class="pid">
-                                {proc.deref()?.title} (PID {proc.deref()?.pid})
-                            </span>
-                        </div>
-                    )),
-                )}
+                                    <td class="pid">
+                                        <span class="pid">
+                                            {proc.deref()?.pid}
+                                        </span>
+                                    </td>
+                                </tr>
+                            )),
+                        )}
+                    </tbody>
+                </table>
             </div>
             <div class="controls">
                 <button
@@ -127,8 +232,54 @@ class TaskManager extends App {
             width: "520px",
             height: `${(360 * window.innerHeight) / 1080}px`,
         });
-        win.content.appendChild(await this.page());
+        const page = await this.page();
+        win.content.appendChild(page);
+
+        createResizableTable(page.querySelector("table.list")!);
 
         return win;
     }
 }
+
+const createResizableTable = function (table: HTMLElement) {
+    const cols = Array.from(table.querySelectorAll("th"))!;
+    cols.pop(); // We don't need to resize the rightmost column
+    cols.forEach.call(cols, function (col: HTMLElement) {
+        // Add a resizer element to the column
+        const resizer = document.createElement("div");
+        resizer.classList.add("resizer");
+
+        // Set the height
+        resizer.style.height = table.offsetHeight + "px";
+
+        col.appendChild(resizer);
+
+        let x = 0;
+        let w = 0;
+
+        const mouseDownHandler = function (e: MouseEvent) {
+            x = e.clientX;
+
+            const styles = window.getComputedStyle(col);
+            w = parseInt(styles.width, 10);
+
+            document.addEventListener("mousemove", mouseMoveHandler);
+            document.addEventListener("mouseup", mouseUpHandler);
+
+            resizer.classList.add("resizing");
+        };
+
+        const mouseMoveHandler = function (e: MouseEvent) {
+            const dx = e.clientX - x;
+            col.style.width = w + dx + "px";
+        };
+
+        const mouseUpHandler = function () {
+            resizer.classList.remove("resizing");
+            document.removeEventListener("mousemove", mouseMoveHandler);
+            document.removeEventListener("mouseup", mouseUpHandler);
+        };
+
+        resizer.addEventListener("mousedown", mouseDownHandler);
+    });
+};
