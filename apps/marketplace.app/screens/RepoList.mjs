1e9e7648ab96f6b417aaccef518ca767ac30a5f0
doctor1 <08armaan21singh@gmail.com>
2025-02-24 07:48:04 -0600

    Update RepoList.mjs

--- apps/marketplace.app/screens/RepoList.mjs
+++ apps/marketplace.app/screens/RepoList.mjs
@@ -212,19 +212,21 @@ export default function RepoList() {
 									url = url + "/";
 								}
 
-								if (!url.startsWith("https://raw.githubusercontent.com/")) {
-									console.warn(
-										"URL does not follow https://raw.githubusercontent.com/[user]/[repo]/master/ format, this is a user skill issue.",
-									);
-									url = url.split("https://github.com")[1];
-									url = "https://raw.githubusercontent.com" + url + "master/";
-								}
-
-                                if (!url.endsWith("master/")) {
-                                    console.warn(
-                                        'URL does not end with "master/", this is a user skill issue.',
-                                    );
-                                    url = url + "/master/"
+                                if (url.includes("github.com") || url.includes("raw.githubusercontent.com")) {
+                                    if (!url.startsWith("https://raw.githubusercontent.com/")) {
+                                        console.warn(
+                                            "URL does not follow https://raw.githubusercontent.com/[user]/[repo]/master/ format, this is a user skill issue.",
+                                        );
+                                        url = url.split("https://github.com")[1];
+                                        url = "https://raw.githubusercontent.com" + url + "master/";
+                                    }
+    
+                                    if (!url.endsWith("master/")) {
+                                        console.warn(
+                                            'URL does not end with "master/", this is a user skill issue.',
+                                        );
+                                        url = url + "/master/"
+                                    }
                                 }
 
 								try {
