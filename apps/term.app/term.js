c32a0bcc58f64c7bf28e19e3d5d1b6ca07e7430f
ProgrammerIn-wonderland <3838shah@gmail.com>
2025-04-14 01:12:51 -0400

    remove onload listener, script is in body anyways

--- apps/term.app/term.js
+++ apps/term.app/term.js
@@ -1,63 +1,61 @@
 const hterm = (await anura.import("anura.hterm")).default;
 const root = document.getElementById("terminal");
 
-window.addEventListener("load", async () => {
-	const t = new hterm.Terminal();
-	t.decorate(root);
-	t.onTerminalReady = async () => {
-		let e = document
-			.querySelector("iframe")
-			.contentDocument.querySelector("x-screen");
-		e.style.overflow = "hidden";
-		let io = t.io.push();
+const t = new hterm.Terminal();
+t.decorate(root);
+t.onTerminalReady = async () => {
+	let e = document
+		.querySelector("iframe")
+		.contentDocument.querySelector("x-screen");
+	e.style.overflow = "hidden";
+	let io = t.io.push();
 
-		t.setBackgroundColor("#141516");
-		t.setCursorColor("#bbb");
+	t.setBackgroundColor("#141516");
+	t.setCursorColor("#bbb");
 
-		if (anura.x86 === undefined) {
-			io.print(
-				"\u001b[33mThe Anura x86 subsystem is not enabled. Please enable it in Settings.\u001b[0m",
-			);
-			return;
-		}
-
-		if (!anura.x86.ready) {
-			io.print(
-				"\u001b[33mThe Anura x86 subsystem has not yet booted. Please wait for the notification that it has booted and try again.\u001b[0m",
-			);
-			return;
-		}
-
-		await io.print(
-			"Welcome to the Anura x86 subsystem.\r\nTo access your Anura files within Linux, use the /root directory.\r\n",
+	if (anura.x86 === undefined) {
+		io.print(
+			"\u001b[33mThe Anura x86 subsystem is not enabled. Please enable it in Settings.\u001b[0m",
 		);
+		return;
+	}
 
-		const pty = await anura.x86.openpty(
-			"/bin/bash --login",
-			t.screenSize.width,
-			t.screenSize.height,
-			(data) => {
-				io.print(data);
-			},
+	if (!anura.x86.ready) {
+		io.print(
+			"\u001b[33mThe Anura x86 subsystem has not yet booted. Please wait for the notification that it has booted and try again.\u001b[0m",
 		);
+		return;
+	}
 
-		function writeData(str) {
-			anura.x86.writepty(pty, str);
-		}
+	await io.print(
+		"Welcome to the Anura x86 subsystem.\r\nTo access your Anura files within Linux, use the /root directory.\r\n",
+	);
 
-		io.onVTKeystroke = writeData;
-		io.sendString = writeData;
+	const pty = await anura.x86.openpty(
+		"/bin/bash --login",
+		t.screenSize.width,
+		t.screenSize.height,
+		(data) => {
+			io.print(data);
+		},
+	);
 
-		io.onTerminalResize = (cols, rows) => {
-			anura.x86.resizepty(pty, cols, rows);
-		};
+	function writeData(str) {
+		anura.x86.writepty(pty, str);
+	}
 
-		instanceWindow.onclose = () => {
-			anura.x86.closepty(pty);
-		};
+	io.onVTKeystroke = writeData;
+	io.sendString = writeData;
 
-		t.installKeyboard();
-
-		root.querySelector("iframe").style.position = "relative";
+	io.onTerminalResize = (cols, rows) => {
+		anura.x86.resizepty(pty, cols, rows);
 	};
-});
+
+	instanceWindow.onclose = () => {
+		anura.x86.closepty(pty);
+	};
+
+	t.installKeyboard();
+
+	root.querySelector("iframe").style.position = "relative";
+};
